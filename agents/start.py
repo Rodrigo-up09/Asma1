6447c31a4f93048df29c82a93203b944381f0b22
Rodrigo Ribeiro <up202206396@g.uporto.pt>
2026-03-05 16:50:02 +0000

    hello world

---
+++ agents/start.py
@@ -0,0 +1,14 @@
+import spade
+
+class DummyAgent(spade.agent.Agent):
+    async def setup(self):
+        print(f"Hello World! I'm agent {self.jid}")
+        
+async def main():
+    dummy = DummyAgent("dummy@localhost", "password")
+    await dummy.start()
+
+if __name__ == "__main__":
+    spade.run(main())
+
+
